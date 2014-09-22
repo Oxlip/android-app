bacb8b855fd8d1f10360745fa20c7e6128e26e89
Samuel Jacob <samueldotj@gmail.com>
2014-09-21 20:50:34 -0700

    Make DeviceListAdapter as private subclass of DeviceListFragment.

--- app/src/main/java/com/getastral/astralmobile/DeviceListFragment.java
+++ app/src/main/java/com/getastral/astralmobile/DeviceListFragment.java
@@ -2,12 +2,20 @@ package com.getastral.astralmobile;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+import java.util.List;
 
 /**
  * A list fragment representing a list of Devices. This fragment
@@ -147,5 +155,69 @@ public class DeviceListFragment extends Fragment {
         }
 
         mActivatedPosition = position;
+    }
+
+    // Adapter for holding devices.
+    public class DeviceListAdapter extends BaseAdapter {
+
+        Context context;
+        List<Device> rowItem;
+
+        DeviceListAdapter(Context context, List<Device> rowItem) {
+            this.context = context;
+            this.rowItem = rowItem;
+        }
+
+        @Override
+        public int getCount() {
+
+            return rowItem.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+
+            return rowItem.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+
+            return rowItem.indexOf(getItem(position));
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            if (convertView == null) {
+                LayoutInflater mInflater = (LayoutInflater) context
+                        .getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+                convertView = mInflater.inflate(R.layout.item_device_list, null);
+            }
+
+            ImageView imgIcon = (ImageView) convertView.findViewById(R.id.dl_image);
+            TextView txtTitle = (TextView) convertView.findViewById(R.id.dl_name);
+            ToggleButton btnOn = (ToggleButton)convertView.findViewById(R.id.dl_btn_on_off);
+            btnOn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ToggleButton btnOn = (ToggleButton)v;
+                    if (!btnOn.isChecked()) {
+                        Log.d("Button", "Sending BLE Turn off message");
+                    } else {
+                        Log.d("Button", "Sending BLE Turn on message");
+                    }
+
+                }
+            });
+
+            Device row_pos = rowItem.get(position);
+            // setting the image resource and title
+            imgIcon.setImageResource(R.drawable.ic_launcher);
+            txtTitle.setText(row_pos.getName());
+
+            return convertView;
+
+        }
     }
 }
