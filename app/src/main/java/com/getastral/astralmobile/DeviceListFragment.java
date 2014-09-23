010f71cd18dd3eaaccb4be48e2ad1b11ab286200
Samuel Jacob <samueldotj@gmail.com>
2014-09-22 22:09:19 -0700

    Added BLE scan to DeviceListFragment.
    Instead of having separate Activity for BLEScan it is easier if it is included in the same screen.
    So I am moving the BleScanActivity to DeviceListFragment.

--- app/src/main/java/com/getastral/astralmobile/DeviceListFragment.java
+++ app/src/main/java/com/getastral/astralmobile/DeviceListFragment.java
@@ -2,8 +2,14 @@ package com.getastral.astralmobile;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,6 +19,7 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import java.util.List;
@@ -27,6 +34,17 @@ import java.util.List;
  * interface.
  */
 public class DeviceListFragment extends Fragment {
+
+    private BluetoothAdapter mBluetoothAdapter;
+    private boolean mScanning;
+    private Handler mHandler;
+
+    private static final int REQUEST_ENABLE_BT = 1;
+    // Stops scanning after 10 seconds.
+    private static final long SCAN_PERIOD = 10000;
+
+    // Adapter to hold all the devices to be displayed.
+    private DeviceListAdapter mListAdapter;
 
     /**
      * The serialization (saved instance state) Bundle key representing the
@@ -75,6 +93,31 @@ public class DeviceListFragment extends Fragment {
     }
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        Activity activity = getActivity();
+        super.onCreate(savedInstanceState);
+        //getActionBar().setTitle(R.string.title_activity_ble_scan);
+        mHandler = new Handler();
+
+        // Use this check to determine whether BLE is supported on the device.  Then you can
+        // selectively disable BLE-related features.
+        if (!activity.getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            Toast.makeText(activity, R.string.ble_not_supported, Toast.LENGTH_SHORT).show();
+            getActivity().finish();
+        }
+        // Initializes a Bluetooth adapter.  For API level 18 and above, get a reference to
+        // BluetoothAdapter through BluetoothManager.
+        final BluetoothManager bluetoothManager = (BluetoothManager) activity.getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = bluetoothManager.getAdapter();
+        // Checks if Bluetooth is supported on the device.
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(activity, R.string.error_bluetooth_not_supported, Toast.LENGTH_SHORT).show();
+            getActivity().finish();
+            return;
+        }
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_device_list, null, false);
     }
@@ -89,6 +132,74 @@ public class DeviceListFragment extends Fragment {
             setActivatedPosition(savedInstanceState.getInt(STATE_ACTIVATED_POSITION));
         }
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // Ensures Bluetooth is enabled on the device.  If Bluetooth is not currently enabled,
+        // fire an intent to display a dialog asking the user to grant permission to enable it.
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+        scanLeDevice(true);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // User chose not to enable Bluetooth.
+        if (requestCode == REQUEST_ENABLE_BT && resultCode == Activity.RESULT_CANCELED) {
+            getActivity().finish();
+            return;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        scanLeDevice(false);
+    }
+
+    private void scanLeDevice(final boolean enable) {
+        if (enable) {
+            // Stops scanning after a pre-defined scan period.
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mScanning = false;
+                    mBluetoothAdapter.stopLeScan(mLeScanCallback);
+                }
+            }, SCAN_PERIOD);
+            mScanning = true;
+            mBluetoothAdapter.startLeScan(mLeScanCallback);
+        } else {
+            mScanning = false;
+            mBluetoothAdapter.stopLeScan(mLeScanCallback);
+        }
+    }
+
+    // Device scan callback.
+    private BluetoothAdapter.LeScanCallback mLeScanCallback = new BluetoothAdapter.LeScanCallback() {
+            @Override
+            public void onLeScan(final BluetoothDevice bleDevice, final int rssi, byte[] scanRecord) {
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        boolean result = mListAdapter.associateBleDevice(bleDevice, rssi);
+                        if (!result) {
+                            Device device = new Device();
+                            device.setName(bleDevice.getName());
+                            device.setUuid(bleDevice.getAddress());
+                            device.setRssi(rssi);
+                            mListAdapter.addDevice(device);
+                        }
+                        Log.d("BLE", bleDevice.getName());
+                        Log.d("BLE", result ? "pass":"fail" );
+                    }
+                });
+            }
+    };
 
     @Override
     public void onSaveInstanceState(Bundle outState) {
@@ -124,9 +235,9 @@ public class DeviceListFragment extends Fragment {
 
         super.onActivityCreated(savedInstanceState);
         DeviceListActivity activity = (DeviceListActivity)getActivity();
-        DeviceListAdapter adapter = new DeviceListAdapter(activity, activity.db.getDevices());
+        mListAdapter = new DeviceListAdapter(activity, activity.db.getDevices());
         ListView listview = (ListView) getView().findViewById(R.id.fdl_list);
-        listview.setAdapter(adapter);
+        listview.setAdapter(mListAdapter);
         listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -166,6 +277,35 @@ public class DeviceListFragment extends Fragment {
         DeviceListAdapter(Context context, List<Device> rowItem) {
             this.context = context;
             this.rowItem = rowItem;
+        }
+
+        /*
+         * Associate the given ble device with matching device uuid.
+         *  Returns true if a device with given uuid is found and bleDevice is associated.
+         */
+        public boolean associateBleDevice(BluetoothDevice bleDevice, int rssi) {
+            for (int i = 0; i < rowItem.size(); i++) {
+                Device device = rowItem.get(i);
+                if (!device.getUuid().equals(bleDevice.getAddress())) {
+                    continue;
+                }
+                device.setBleDevice(bleDevice);
+                if (device.getRssi() != rssi) {
+                    device.setRssi(rssi);
+                    this.notifyDataSetInvalidated();
+                }
+                return true;
+            }
+            return false;
+        }
+
+        /**
+         * Add newly discovered device.
+         * @param device - New device.
+         */
+        public void addDevice(Device device) {
+            rowItem.add(device);
+            this.notifyDataSetInvalidated();
         }
 
         @Override
@@ -217,7 +357,6 @@ public class DeviceListFragment extends Fragment {
             txtTitle.setText(row_pos.getName());
 
             return convertView;
-
         }
     }
 }
