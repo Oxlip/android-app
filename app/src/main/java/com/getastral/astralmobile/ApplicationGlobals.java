a5a32f93d2f04c14c4d1d7c29011aca4e74221ac
Samuel Jacob <samueldotj@gmail.com>
2014-10-25 09:43:01 -0700

    Added ApplicationGlobals class.
    This singleton class will hold all the application global variables.
     Currently it is holding Context.

---
+++ app/src/main/java/com/getastral/astralmobile/ApplicationGlobals.java
@@ -0,0 +1,18 @@
+package com.getastral.astralmobile;
+
+import android.app.Application;
+import android.content.Context;
+
+public class ApplicationGlobals extends Application {
+
+    private static Context context;
+
+    public void onCreate(){
+        super.onCreate();
+        ApplicationGlobals.context = getApplicationContext();
+    }
+
+    public static Context getAppContext() {
+        return ApplicationGlobals.context;
+    }
+}
