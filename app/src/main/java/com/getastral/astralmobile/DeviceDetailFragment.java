16b72cdf08190034d932127b71d6ff852d6336a2
Samuel Jacob <samueldotj@gmail.com>
2014-10-29 21:33:12 -0700

    Added Save button for DeviceDetail fragment.
    This button will save and close the device detail fragment.

--- app/src/main/java/com/getastral/astralmobile/DeviceDetailFragment.java
+++ app/src/main/java/com/getastral/astralmobile/DeviceDetailFragment.java
@@ -1,15 +1,16 @@
 package com.getastral.astralmobile;
 
 import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
-import android.widget.TextView;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -39,33 +40,62 @@ public class DeviceDetailFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_device_detail, container, false);
-        TextView txtView = (TextView) rootView.findViewById(R.id.dd_name);
-        Spinner spinner = (Spinner) rootView.findViewById(R.id.dd_lst_connected_device);
-        Button btn = (Button) rootView.findViewById(R.id.dd_btn_test_data);
+        final EditText txtName = (EditText) rootView.findViewById(R.id.dd_name);
+        final Spinner sprApplianceType = (Spinner) rootView.findViewById(R.id.dd_lst_connected_device);
+        Button btnTest = (Button) rootView.findViewById(R.id.dd_btn_test_data);
+        Button btnSave = (Button) rootView.findViewById(R.id.dd_btn_save);
         ListView lstDeviceData = (ListView) rootView.findViewById(R.id.dd_lst_device_data);
         String deviceAddress = this.getArguments().getString("deviceAddress");
+        final DatabaseHelper.DeviceInfo deviceInfo = DatabaseHelper.getDeviceInfo(deviceAddress);
         Calendar c = Calendar.getInstance();
         c.add(Calendar.MONTH, -1);
 
         List<DatabaseHelper.ApplianceType> applianceTypeList = DatabaseHelper.getApplianceTypeList();
 
         final ArrayAdapter<DatabaseHelper.ApplianceType> adapter =
-                new ArrayAdapter<DatabaseHelper.ApplianceType>(getActivity().getApplicationContext(),
-                        android.R.layout.simple_spinner_dropdown_item, applianceTypeList);
+                new ArrayAdapter<DatabaseHelper.ApplianceType>(getActivity(), android.R.layout.simple_spinner_dropdown_item, applianceTypeList);
 
         // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
-        spinner.setAdapter(adapter);
+        sprApplianceType.setAdapter(adapter);
 
-        txtView.setText(deviceAddress);
-
-        ArrayAdapter<DatabaseHelper.DeviceData> ddAdapter = new ArrayAdapter<DatabaseHelper.DeviceData>(getActivity().getApplicationContext(),
+        ArrayAdapter<DatabaseHelper.DeviceData> ddAdapter = new ArrayAdapter<DatabaseHelper.DeviceData>(getActivity(),
                 android.R.layout.simple_list_item_1, DatabaseHelper.getDeviceDataListForDateRange(deviceAddress, c.getTime(), 31));
-
         lstDeviceData.setAdapter(ddAdapter);
-        btn.setTag(deviceAddress);
-        btn.setOnClickListener(new View.OnClickListener() {
+
+        txtName.setText(deviceInfo.name);
+        int position = 0;
+        if (deviceInfo.applianceType != null) {
+            for (DatabaseHelper.ApplianceType applianceType: applianceTypeList) {
+                if (applianceType.name.equals(deviceInfo.applianceType)) {
+                    break;
+                }
+                position++;
+            }
+        }
+
+        sprApplianceType.setSelection(position);
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                deviceInfo.name = txtName.getText().toString();
+                deviceInfo.applianceType = sprApplianceType.getSelectedItem().toString();
+                DatabaseHelper.saveDeviceInfo(deviceInfo);
+
+                // close this fragment and go back.
+                FragmentManager fm = getFragmentManager();
+                if (fm.getBackStackEntryCount() > 0) {
+                    fm.popBackStack();
+                } else {
+                    getActivity().onBackPressed();
+                }
+            }
+        });
+
+        btnTest.setTag(deviceAddress);
+        btnTest.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Calendar c = Calendar.getInstance();
