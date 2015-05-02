0697bec597c046ffb907cfd712a9a1d2f9ee84cb
Samuel Jacob <samueldotj@gmail.com>
2015-05-02 16:03:03 -0700

    Create fragment for Lyra.

---
+++ Nuton/src/main/java/com/nuton/mobile/LyraDetailFragment.java
@@ -0,0 +1,36 @@
+package com.nuton.mobile;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A fragment representing a single Device detail screen.
+ * This fragment is either contained in a {@link DeviceListActivity}
+ * in two-pane mode (on tablets) or a {@link DeviceDetailActivity}
+ * on handsets.
+ */
+public class LyraDetailFragment extends DetailFragment {
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public LyraDetailFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.fragment_lyra_detail, container, false);
+
+        return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+}
